5fba130ddd639dbb7dde39b8820c5c77820b1848
T3Marius <168598779+T3Marius@users.noreply.github.com>
2024-08-30 03:56:04 +0300

    Add files via upload

---
+++ src/Config/config.cs
@@ -0,0 +1,76 @@
+﻿using CounterStrikeSharp.API;
+using System.Reflection;
+using Tomlyn.Model;
+using Tomlyn;
+using CounterStrikeSharp.API.Core.Translations;
+
+namespace MenuManager
+{
+    public static class Config
+    {
+        public static Cfg ConfigData { get; set; } = new Cfg();
+
+        public static void Load()
+        {
+            string assemblyName = Assembly.GetExecutingAssembly().GetName().Name ?? "";
+            string cfgPath = $"{Server.GameDirectory}/csgo/addons/counterstrikesharp/configs/plugins/{assemblyName}";
+
+            LoadConfig($"{cfgPath}/config.toml");
+        }
+
+        private static void LoadConfig(string configPath)
+        {
+            if (!File.Exists(configPath))
+            {
+                throw new FileNotFoundException($"Configuration file not found: {configPath}");
+            }
+
+            string configText = File.ReadAllText(configPath);
+            TomlTable model = Toml.ToModel(configText);
+
+            string commandName = model.TryGetValue("CommandName", out var cmd) ? cmd.ToString()! : "css_menu";
+            string messageTag = model.TryGetValue("MessageTag", out var tag) ? tag.ToString()! : "[MenuManager]";
+            Config_MenuOptions menuOptions = new Config_MenuOptions();
+
+            if (model.TryGetValue("MenuOptions", out var menuOptionsSection))
+            {
+                foreach (var entry in (TomlTable)menuOptionsSection)
+                {
+                    var optionTable = (TomlTable)entry.Value;
+                    menuOptions.Options.Add(new MenuOption
+                    {
+                        Name = optionTable["Name"].ToString()!,
+                        Command = optionTable["Command"].ToString()!,
+                        OpeningMessage = optionTable.TryGetValue("OpeningMessage", out var msg)
+                ? StringExtensions.ReplaceColorTags(msg.ToString()!)
+                : "Opening {option}..."
+                    });
+                }
+            }
+
+            ConfigData = new Cfg
+            {
+                CommandName = commandName,
+                MenuOptions = menuOptions
+            };
+        }
+
+        public class Cfg
+        {
+            public string CommandName { get; set; } = "css_menu";
+            public Config_MenuOptions MenuOptions { get; set; } = new();
+        }
+
+        public class Config_MenuOptions
+        {
+            public List<MenuOption> Options { get; set; } = new();
+        }
+
+        public class MenuOption
+        {
+            public string Name { get; set; } = string.Empty;
+            public string Command { get; set; } = string.Empty;
+            public string OpeningMessage { get; set; } = "Opening {option}...";
+        }
+    }
+}
---
+++ src/MenuManager.cs
@@ -0,0 +1,56 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Core.Capabilities;
+using CounterStrikeSharp.API.Modules.Commands;
+using WASDSharedAPI;
+
+namespace MenuManager
+{
+    public class AllInOne : BasePlugin
+    {
+        public override string ModuleName => "MenuManager";
+        public override string ModuleVersion => "1.0.0";
+        public override string ModuleAuthor => "T3Marius";
+
+        public static IWasdMenuManager? MenuManager;
+
+        public override void Load(bool hotReload)
+        {
+            Config.Load();
+            foreach (var command in Config.ConfigData.CommandName.Split(','))
+            {
+                AddCommand($"css_{command}", "opens menu", Menu);
+            }
+            
+        }
+        public IWasdMenuManager? GetMenuManager()
+        {
+            if (MenuManager == null)
+                MenuManager = new PluginCapability<IWasdMenuManager>("wasdmenu:manager").Get();
+
+            return MenuManager; 
+        }
+
+        public void Menu(CCSPlayerController? player, CommandInfo? info)
+        {
+            var manager = GetMenuManager();
+            if (manager == null)
+                return;
+
+            IWasdMenu menu = manager.CreateMenu("Menu");
+
+            foreach (var option in Config.ConfigData.MenuOptions.Options)
+            {
+                menu.Add(option.Name, (p, opt) =>
+                {
+                    p.ExecuteClientCommandFromServer(option.Command);
+                    p.PrintToChat($"{Localizer["prefix"]} {option.OpeningMessage.Replace("{option}", option.Name)}");
+                    manager.CloseMenu(p);
+                });
+            }
+
+            manager.OpenMainMenu(player, menu);
+        }
+    }
+}
